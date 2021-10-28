80a129475e4d2aec004ee559b66857ed476ffc66
Edwin Dalorzo <edwin@route.com>
2021-10-28 11:56:49 -0600

    More experiments with Windows!

--- main.cpp
+++ main.cpp
@@ -15,7 +15,50 @@ using namespace std;
 void center(int row, char *title);
 int kbhit();
 
+// windows of smaller size
 int main() {
+
+    WINDOW *tiny;
+    initscr();
+    start_color();
+
+    // configure colors
+    init_pair(1,COLOR_WHITE, COLOR_BLUE);
+    init_pair(2, COLOR_WHITE, COLOR_RED);
+
+    // create tiny window
+    tiny = newwin(LINES/2, COLS/2, LINES/4, COLS/4);
+    if(!tiny) {
+        endwin();
+        puts("Unable to create window");
+        return 1;
+    }
+
+    wbkgd(tiny, COLOR_PAIR(2));
+    waddstr(tiny, "This is a tiny window\n");
+
+    // standard screen
+    bkgd(COLOR_PAIR(1));
+    addstr("This is the standard screen\n");
+    addstr("Press Enter ");
+    refresh();
+    getch();
+    // show second window
+    wrefresh(tiny);
+    getch();
+
+    // show standard window again
+    touchwin(stdscr);
+    refresh();
+    getch();
+
+    endwin();
+    return 0;
+
+}
+
+// draws a grid with numbers with different properties
+int basic54() {
     initscr();
 
     start_color();
