b41f96487c5f1645cc733a2098ad87f6e88f0ff6
Edwin Dalorzo <edwin@route.com>
2021-10-27 22:09:24 -0600

    Completes exercises up to chapter 8.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #define _XOPEN_SOURCE_EXTENDED 1
+
 #include<ncurses.h>
 #include<clocale>
 #include<cstring>
@@ -9,25 +10,378 @@
 using namespace std;
 
 void center(int row, char *title);
+int kbhit();
 
 int main() {
+    return 0;
+}
+
+// flushing input
+int basic50() {
+    char buffer[81];
+    initscr();
+
+    addstr("Type. I'll wait...\n");
+    refresh();
+    napms(5000); // 5 seconds
+
+    addstr("Flushing buffer.\n");
+    flushinp();
+    addstr("Here is what you typed:\n");
+    getnstr(buffer, 80);
+
+    endwin();
+    return 0;
+}
+
+// silent input e.g. for a password capture
+int basic49() {
+    char name[32];
+    char password[32];
+
+    initscr();
+
+    addstr("Name: ");
+    getnstr(name, 31);
+    /* disable text output */
+    noecho();
+    addstr("Password: ");
+    getnstr(password, 31);
+    /* enable text output */
+    echo();
+    printw("%s's password is '%s'", name, password);
+    refresh();
+    getch();
+    endwin();
+
+    return 0;
+}
+
+// using the keypad function to test characters pressed
+int basic48() {
+
+    int ch;
+
+    initscr();
+    keypad(stdscr, TRUE);
+    do {
+        ch = getch();
+        switch (ch) {
+            case KEY_DOWN:
+                addstr("Down\n");
+                break;
+            case KEY_UP:
+                addstr("Up\n");
+                break;
+            case KEY_LEFT:
+                addstr("Left\n");
+                break;
+            case KEY_RIGHT:
+                addstr("Right\n");
+                break;
+            default:
+                break;
+        }
+        refresh();
+    } while (ch != '\n');
+
+    endwin();
+    return 0;
+
+
+}
+
+// using th kbhit function to unread a character
+int basic47() {
+    int x;
+
+    initscr();
+    addstr("Tap a key while I count....\n");
+    for (x = 1; x < 21; x++) {
+        printw("%d ", x);
+        refresh();
+        napms(500);
+        if (kbhit()) {
+            break;
+        }
+    }
+    addstr("\nDone!\n");
+    printw("You pressed the '%c' key\n", getch());
+    refresh();
+    getch();
+
+    endwin();
+    return 0;
+
+}
+
+
+// check the keyboard queue
+int kbhit() {
+    int ch, r;
+
+    // turn off blocking and echo
+    nodelay(stdscr, TRUE);
+    noecho();
+
+    // check for input
+    ch = getch();
+    if (ch == ERR) // no input
+        r = FALSE;
+    else {
+        r = TRUE;
+        // return key to the queue
+        ungetch(ch);
+    }
+    // restore block and echo
+    echo();
+    nodelay(stdscr, FALSE);
+    return r;
+}
+
+/**
+ * getch does not echo
+ */
+int basic46() {
+    int key1, key2;
+    initscr();
+
+    addstr("Type a key: ");
+    refresh();
+    key1 = getch();
+    clear();
+
+    addstr("Type the same key: ");
+    noecho();
+    key2 = getch();
+    move(1, 0);
+    if (key1 == key2) {
+        addstr("The keys match");
+    } else {
+        addstr("The keys don't match");
+    }
+    getch();
+    refresh();
+
+    endwin();
+    return 0;
+
+}
+
+
+/**
+ * getch does not block
+ */
+int basic45() {
+    int value = 0;
+
+    initscr();
+
+    addstr("Press any key to begin:\n");
+    refresh();
+    getch();
+
+    /* turn off get blocking */
+    nodelay(stdscr, TRUE);
+    addstr("Press any key to stop the loop!\n");
+    while (getch() == ERR) {
+        printw("%d\r", value++);
+        refresh();
+    }
+
+    endwin();
+    return 0;
+
+}
+
+
+int basic42() {
+    char name[32];
+    int ch;
+
+    initscr();
+    do {
+        clear();
+        addstr("Enter your name: ");
+        getnstr(name, 32);
+        move(1, 0);
+        printw("Your name is %s. ", name);
+        printw("Is this correct? ");
+        ch = getch();
+    } while (ch != 'y');
+    move(2, 0);
+    printw("Pleased to you, %s\n", name);
+    getch();
+
+    endwin();
+    return 0;
+}
+
+int basic41() {
+    int c, y, x, cmax;
+
+    initscr();
+    getmaxyx(stdscr, y, x);
+    cmax = (x * y / 5);
+    cmax = (x * y / 5);
+    for (c = 0; c < cmax; c++) {
+        addstr("blah");
+    }
+    getch();
+
+    move(5, 20);
+    //clrtoeol(); // clear to end of line
+    clrtobot(); // clear to bottom
+    refresh();
+    getch();
+
+    endwin();
+    return 0;
+
+
+}
+
+int basic40() {
+    int c, y, x, cmax;
+
+    initscr();
+    getmaxyx(stdscr, y, x);
+    refresh();
+    cmax = (x * y / 5);
+    for (c = 0; c < cmax; c++) {
+        addstr("blah");
+    }
+    getch();
+
+    clear();
+    refresh();
+    getch();
+
+    endwin();
+    return 0;
+
+}
+
+int basic39() {
+    int row, col;
+
+    initscr();
+
+    for (row = 0; row < LINES; row++) {
+        for (col = 0; col < COLS; col++) {
+            addch('.');
+        }
+    }
+    refresh();
+    getch();
+
+    move(5, 0);
+    insdelln(3);
+    refresh();
+    getch();
+
+    insdelln(-5);
+    refresh();
+    getch();
+
+    endwin();
+    return 0;
+}
+
+int basic38() {
+    int x;
+
+    initscr();
+    addstr("This is the first line\n");
+    addstr("Line two here\n");
+    addstr("The third line\n");
+    addstr("Fourth line here\n");
+    addstr("And the fifth line\n");
+    refresh();
+    getch();
+
+    move(3, 7);
+    for (x = 0; x < 5; x++) {
+        delch();
+        refresh();
+        napms(250);
+    }
+    getch();
+    endwin();
+    return 0;
+}
+
+int basic37() {
+    initscr();
+
+    // add fine lines
+    addstr("This is the first line\n");
+    addstr("Line two here\n");
+    addstr("The third line\n");
+    addstr("Fourth line here\n");
+    addstr("And the fifth line\n");
+    refresh();
+    getch();
+
+    // remove a line
+    move(2, 0);
+    deleteln();
+    refresh();
+    getch();
+
+    endwin();
+    return 0;
+}
+
+
+int basic36() {
+    char text1[] = "This is the first line\n";
+    char text2[] = "Line two here\n";
+    char text3[] = "The third line\n";
+    char text4[] = "Fourth line here\n";
+    char text5[] = "And the fifth line\n";
+
+    initscr();
+    addstr(text1);
+    addstr(text3);
+    addstr(text5);
+    refresh();
+    getch();
+
+    move(1, 0);
+    insertln();
+    insstr(text2);
+    refresh();
+    getch();
+
+    move(3, 0);
+    deleteln();
+    refresh();
+    getch();
+
+    endwin();
+    return 0;
+}
+
+
+int basic35() {
     char text[] = "Armstrong walks on moon!";
     char *t;
     char a;
-    int len;
+    size_t len;
 
     initscr();
     move(0, 5);
-    for(a = 'A'; a < 'Z' + 1; a++) {
+    for (a = 'A'; a < 'Z' + 1; a++) {
         addch(a);
         addstr(" ");
     }
 
     len = strlen(text);
     t = text;
-    while(len) {
-        move(5,5);
-        insch(*(t+len-1));
+    while (len) {
+        move(5, 5);
+        insch(*(t + len - 1));
         refresh();
         napms(100);
         len--;
@@ -38,20 +392,19 @@ int main() {
     return 0;
 
 
-
 }
 
 int basic34() {
     char text[] = "Armstrong walks on moon!";
     char *t;
-    int len;
+    size_t len;
 
     initscr();
     len = strlen(text);
     t = text;
-    while(len) {
-        move(5,5);
-        insch(*(t+len-1));
+    while (len) {
+        move(5, 5);
+        insch(*(t + len - 1));
         refresh();
         napms(100);
         len--;
@@ -78,7 +431,7 @@ int basic33() {
     refresh();
     getch();
 
-    move(1,0);
+    move(1, 0);
     insertln();
     refresh();
     getch();
@@ -87,18 +440,14 @@ int basic33() {
     refresh();
     getch();
 
-    move(3,0);
+    move(3, 0);
     insertln();
     addstr(text4);
     refresh();
     getch();
 
-
     endwin();
     return 0;
-
-
-
 }
 
 int basic32() {
@@ -110,8 +459,7 @@ int basic32() {
     addstr("Type some text; '~' to end:\n");
     refresh();
 
-    while((ch = getch()) != '~')
-        ;
+    while ((ch = getch()) != '~');
 
     getyx(stdscr, row, col);
     printw("\n\nThe cursor was at row %d, column %d when you stopped typing", row, col);
@@ -122,7 +470,6 @@ int basic32() {
 
     return 0;
 }
-
 
 
 int basic31() {
@@ -142,8 +489,9 @@ int basic31() {
     return 0;
 }
 
-void center(int row, char* title) {
-    int len, indent, y, width;
+void center(int row, char *title) {
+    size_t len, indent;
+    int y, width;
     // get screen width
     getmaxyx(stdscr, y, width);
     // get title length
@@ -163,7 +511,7 @@ int basic30() {
     lines--;
     cols--;
 
-    mvaddch(0,0, '*');
+    mvaddch(0, 0, '*');
     refresh();
     napms(500);
 
@@ -183,12 +531,12 @@ int basic30() {
     return 0;
 }
 
-int basic29(){
-    int x,y;
+int basic29() {
+    int x, y;
     initscr();
     getmaxyx(stdscr, y, x);
-    printw("Window is %d rows by %d columns.\n", y,x);
-    printw("Window is %d rows by %d columns.\n", LINES,COLS);
+    printw("Window is %d rows by %d columns.\n", y, x);
+    printw("Window is %d rows by %d columns.\n", LINES, COLS);
     refresh();
     getch();
     endwin();
@@ -202,7 +550,7 @@ int basic28() {
             0x0432, 0x0435, 0x0442,
             0x0021, 0x0
     };
-    setlocale(LC_CTYPE,"en_US.UTF-8");
+    setlocale(LC_CTYPE, "en_US.UTF-8");
     initscr();
     addwstr(hello);
     refresh();
@@ -219,7 +567,7 @@ int basic27() {
             A_NORMAL,
             L"\u2665"
     };
-    setlocale(LC_CTYPE,"en_US.UTF-8");
+    setlocale(LC_CTYPE, "en_US.UTF-8");
     initscr();
     addstr("I ");
     add_wch(&heart);
@@ -245,7 +593,8 @@ int basic26() {
 
 }
 
-int basic25() {
+//TODO: This one didn't work for me! Why?
+int basic24() {
     chtype string[12] = {
             ACS_ULCORNER, ACS_HLINE, ACS_URCORNER, '\n',
             ACS_VLINE, ' ', ACS_VLINE, '\n',
@@ -261,7 +610,7 @@ int basic25() {
 
 }
 
-int basic24() {
+int basic23() {
     chtype string[] = {
             'H' | A_BOLD,
             'e',
@@ -285,8 +634,9 @@ int basic24() {
     return 0;
 }
 
-int basic23() {
+int basic22() {
     initscr();
+
     addch(ACS_ULCORNER);
     addch(ACS_HLINE);
     addch(ACS_URCORNER);
@@ -306,7 +656,7 @@ int basic23() {
     return 0;
 }
 
-int basic22() {
+int basic21() {
     int a;
     initscr();
     for (a = 0; a < 127; a++) {
@@ -319,47 +669,66 @@ int basic22() {
     return 0;
 }
 
-int basic21() {
+int basic20() {
     initscr();
+
     addch(A_ALTCHARSET | ACS_PI);
     addstr("= 3.14159");
+
     addch(A_ALTCHARSET | ACS_DARROW);
     refresh();
+
     getch();
     endwin();
     return 0;
 }
 
-int basic20() {
+int basic19() {
+
     initscr();
+
     attrset(A_STANDOUT);
     addstr("This is A_STANDOUT\n");
+
     attrset(A_UNDERLINE);
     addstr("This is A_UNDERLINE\n");
+
     attrset(A_REVERSE);
     addstr("This is A_REVERSE\n");
+
     attrset(A_BLINK);
     addstr("This is A_BLINK\n");
+
     attrset(A_DIM);
     addstr("This is A_DIM\n");
+
     attrset(A_BOLD);
     addstr("This is A_BOLD\n");
+
     attrset(A_ALTCHARSET);
     addstr("This is A_ALTCHARSET\n");
+
     attrset(A_INVIS);
     addstr("This is A_INVIS\n");
+
     attrset(A_PROTECT);
     addstr("This is A_PROTECT\n");
+
     attrset(A_HORIZONTAL);
     addstr("This is A_HORIZONTAL\n");
+
     attrset(A_LEFT);
     addstr("This is A_LEFT\n");
+
     attrset(A_LOW);
     addstr("This is A_LOW\n");
+
     attrset(A_RIGHT);
     addstr("This is A_RIGHT\n");
+
     attrset(A_TOP);
     addstr("This is A_TOP\n");
+
     attrset(A_VERTICAL);
     addstr("This is A_VERTICAL\n");
 
@@ -370,17 +739,23 @@ int basic20() {
 
 }
 
-int basic19() {
+int basic18() {
+
     initscr();
+
     addch('c');
     addch(A_BOLD | 'a');
     addch(A_REVERSE | 't');
+    addch('s' | A_BLINK);
+    addch('\n');
     refresh();
     getch();
 
     move(0, 2);
     addch('r');
+    move(1, 0);
     refresh();
+
     getch();
 
     endwin();
@@ -389,7 +764,7 @@ int basic19() {
 
 }
 
-int basic18() {
+int basic17() {
     initscr();
     addch('c');
     addch(A_BOLD | 'a');
@@ -466,8 +841,10 @@ int basic13() {
     if (!can_change_color()) {
         addstr("This probably won't work...\n");
     }
+
     init_color(PINK, 1000, 750, 750);
     init_pair(1, PINK, COLOR_BLACK);
+
     attrset(COLOR_PAIR(1));
     printw("This is the new color %d\n", PINK);
     refresh();
@@ -482,14 +859,19 @@ int basic12() {
     start_color();
     init_pair(1, COLOR_BLACK, COLOR_RED);
     init_pair(2, COLOR_YELLOW, COLOR_BLACK);
+
     attrset(COLOR_PAIR(1));
     addstr("I am Mr. Black!\n");
+
     attrset(COLOR_PAIR(2));
     addstr("I am Mr. Yellow!\n");
+
     attrset(COLOR_PAIR(1) | A_BOLD);
     addstr("I'm feeling bold!\n");
+
     attrset(COLOR_PAIR(2) | A_BOLD);
     addstr("Me too!");
+
     refresh();
     getch();
     endwin();
@@ -508,6 +890,7 @@ int basic11() {
         puts("Unable to start colors!");
         return 1;
     }
+    // COLORES ARE WORKING YAY!!!
     printw("Colors initialized.\n");
     printw("%d colors available.\n", COLORS);
     printw("%d color pairs.", COLOR_PAIRS);
@@ -562,17 +945,19 @@ int basic09() {
 }
 
 int basic08() {
+
     int pieces;
     const float uni = 4.5;
+
     initscr();
     addstr("SUSHI BAR");
     move(2, 0);
     printw("We have Uni today for $%.2f\n", uni);
-    addstr("How many pieces would you like?");
+    addstr("How many pieces would you like? ");
     refresh();
     char int_conv[3] = "%d";
     scanw(int_conv, &pieces);
-    printw("You want %d pieces?\n", pieces);
+    printw("You want %d pieces\n", pieces);
     printw("That will be $%.2f", uni * (float) pieces);
     refresh();
     getch();
@@ -605,8 +990,7 @@ int basic06() {
     addstr("Press ~ to quit\n");
     refresh();
 
-    while ((ch = getch()) != '~') {
-
+    while ((ch = getch()) != '~') { ;
     }
     endwin();
     return 0;
@@ -628,7 +1012,7 @@ int basic05() {
 }
 
 int basic04() {
-    char t1[] = "Shall I compare thee";
+    char t1[] = "Shall I compare thee ";
     char t2[] = "to a summer's day";
     initscr();
     addstr(t1);
@@ -642,7 +1026,7 @@ int basic04() {
 }
 
 int basic03() {
-    char t1[] = "Shall I compare thee";
+    char t1[] = "Shall I compare thee ";
     char t2[] = "to a summer's day";
     initscr();
     addstr(t1);
@@ -654,7 +1038,7 @@ int basic03() {
 }
 
 int basic02() {
-    char text[] = "Greetings from Ncurses!";
+    char text[] = "Greetings from Ncurses!"; //null-terminated string
     char *t;
 
     initscr();
